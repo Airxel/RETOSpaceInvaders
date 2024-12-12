ead65dbae34dbbbd566e3c7b8bc15c1d07c8ece7
Airxel <airxeldroide@gmail.com>
2024-12-12 11:23:32 +0000

    Initial Commit

---
+++ RETO Space Invaders/Assets/Scripts/AnimationsManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationsManager : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject mainShip1, mainShip2, fastShip1, fastShip2, balancedShip1, balancedShip2, slowShip1, slowShip2;
+
+    [SerializeField]
+    GameObject mainShipCollection, fastShipCollection, balancedShipCollection, slowShipCollection;
+
+    [SerializeField]
+    private float modelChangeTime = 1f;
+    private float modelChangeTimer = 0f;
+
+    private bool modelChanging = true;
+
+    private void Start()
+    {
+        mainShip1.SetActive(true);
+        mainShip2.SetActive(false);
+
+        LeanTween.moveY(mainShipCollection, 10f, 1f).setEase(LeanTweenType.easeInOutSine).setLoopPingPong();
+        LeanTween.moveY(fastShipCollection, 55f, 0.5f).setEase(LeanTweenType.easeInOutSine).setLoopPingPong();
+        LeanTween.moveY(balancedShipCollection, 55f, 1f).setEase(LeanTweenType.easeInOutSine).setLoopPingPong();
+        LeanTween.moveY(slowShipCollection, 55f, 1.5f).setEase(LeanTweenType.easeInOutSine).setLoopPingPong();
+    }
+
+    private void Update()
+    {
+        modelChangeTimer = modelChangeTimer + Time.deltaTime;
+
+        if (modelChangeTimer >= modelChangeTime)
+        {
+            modelChanging = !modelChanging;
+
+            mainShip1.SetActive(modelChanging);
+            mainShip2.SetActive(!modelChanging);
+
+            fastShip1.SetActive(modelChanging);
+            fastShip2.SetActive(!modelChanging);
+
+            balancedShip1.SetActive(modelChanging);
+            balancedShip2.SetActive(!modelChanging);
+
+            slowShip1.SetActive(modelChanging);
+            slowShip2.SetActive(!modelChanging);
+
+            modelChangeTimer = 0f;
+        }
+    }
+}
