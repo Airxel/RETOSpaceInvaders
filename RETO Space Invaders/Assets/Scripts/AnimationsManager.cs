88102814167e93dd336ace357bcea4ab1409232f
Airxel <airxeldroide@gmail.com>
2024-12-18 23:15:16 +0000

    Tercer Día Noche

--- RETO Space Invaders/Assets/Scripts/AnimationsManager.cs
+++ RETO Space Invaders/Assets/Scripts/AnimationsManager.cs
@@ -9,7 +9,10 @@ public class AnimationsManager : MonoBehaviour
     GameObject mainShip1, mainShip2, fastShip1, fastShip2, balancedShip1, balancedShip2, slowShip1, slowShip2;
 
     [SerializeField]
-    GameObject mainShipCollection, fastShipCollection, balancedShipCollection, slowShipCollection;
+    GameObject leftShelter1, leftShelter2, middleLeftShelter1, middleLeftShelter2, middleRightShelter1, middleRightShelter2, rightShelter1, rightShelter2;
+
+    [SerializeField]
+    GameObject fastShipCollection, balancedShipCollection, slowShipCollection;
 
     [SerializeField]
     private float modelChangeTime = 1f;
@@ -46,6 +49,18 @@ public class AnimationsManager : MonoBehaviour
 
             slowShip1.SetActive(modelChanging);
             slowShip2.SetActive(!modelChanging);
+
+            leftShelter1.SetActive(modelChanging);
+            leftShelter2.SetActive(!modelChanging);
+
+            middleLeftShelter1.SetActive(modelChanging);
+            middleLeftShelter2.SetActive(!modelChanging);
+
+            middleRightShelter1.SetActive(modelChanging);
+            middleRightShelter2.SetActive(!modelChanging);
+
+            rightShelter1.SetActive(modelChanging);
+            rightShelter2.SetActive(!modelChanging);
 
             modelChangeTimer = 0f;
         }
